58f083d9446895e9bf93b3082c2f708c4987eb6b
oxunov_713 <aoxunov713@gmail.com>
2023-08-08 11:33:01 +0500

    get qildim

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as myHTTP;
 
 Future<void> main() async {
   String baseUrl = "https://dummyjson.com";
-  Uri url = Uri.parse("$baseUrl/todos/6");
-  myHTTP.Response responseDel = await myHTTP.delete(url);
-  print(responseDel.body);
+  Uri url = Uri.parse("$baseUrl/todos");
+  myHTTP.Response responseGet = await myHTTP.get(url);
+  print(responseGet.body);
 }
