fb902ae807849ab4277e7dade628430a7f31716e
oxunov_713 <aoxunov713@gmail.com>
2023-08-08 11:30:25 +0500

    put qildim

--- bin/main.dart
+++ bin/main.dart
@@ -2,18 +2,13 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as myHTTP;
 
-import 'package:main/class/todos.dart';
-
 Future<void> main() async {
   String baseUrl = "https://dummyjson.com";
-  Uri url = Uri.parse("$baseUrl/todos/add");
-
-  Todos todo = Todos( todo: "sds", completed: true, userId: 50);
-//! post
-  myHTTP.Response responsePost = await myHTTP.post(
-    url,
-    headers: <String, String>{"Content-Type": "application/json"},
-    body: jsonEncode(todo.toJson()),
-  );
-  print(responsePost.body);
+  Uri url = Uri.parse("$baseUrl/todos/6");
+  myHTTP.Response responsePut = await myHTTP.put(url,
+      headers: <String, String>{
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: jsonEncode({'comleted': 'true'}));
+  print(responsePut.body);
 }
