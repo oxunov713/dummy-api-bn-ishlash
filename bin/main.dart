e1ac0ae3058e3787c62deb59788682512ca93d3e
oxunov_713 <aoxunov713@gmail.com>
2023-08-08 11:34:27 +0500

    limit and skip ni ishlatib ko'rdim

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as myHTTP;
 
 Future<void> main() async {
   String baseUrl = "https://dummyjson.com";
-  Uri url = Uri.parse("$baseUrl/todos/2");
+  Uri url = Uri.parse("$baseUrl/todos/?limit=5&skip=140");
   myHTTP.Response responseGet = await myHTTP.get(url);
   print(responseGet.body);
 }
