29f6474ae6c9da96fc4ec14b9934099c9ff82679
oxunov_713 <aoxunov713@gmail.com>
2023-08-08 11:32:00 +0500

    delete qildim

--- bin/main.dart
+++ bin/main.dart
@@ -5,10 +5,6 @@ import 'package:http/http.dart' as myHTTP;
 Future<void> main() async {
   String baseUrl = "https://dummyjson.com";
   Uri url = Uri.parse("$baseUrl/todos/6");
-  myHTTP.Response responsePut = await myHTTP.put(url,
-      headers: <String, String>{
-        "Content-Type": "application/json; charset=UTF-8"
-      },
-      body: jsonEncode({'comleted': 'true'}));
-  print(responsePut.body);
+  myHTTP.Response responseDel = await myHTTP.delete(url);
+  print(responseDel.body);
 }
