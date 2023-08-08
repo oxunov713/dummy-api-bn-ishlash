fd6f07ed9af06b38ce26f80b782677c339f3fecd
oxunov_713 <aoxunov713@gmail.com>
2023-08-08 10:09:25 +0500

    todosga list qoshdim

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,18 @@
-import 'package:http/http.dart' as myHTTP;
-void main() {
-  String baseUrl = "https://dummyjson.com/todos";
-  Uri uri = Uri.parse(baseUrl);
+import 'dart:convert';
 
-  List.
+import 'package:http/http.dart' as myHTTP;
+import 'package:main/class/dummy.dart';
+
+void main() async {
+  String baseUrl = "https://dummyjson.com/todos";
+  Uri url = Uri.parse(baseUrl);
+
+  myHTTP.Response response = await myHTTP.get(url);
+
+  List<Dummy> mydummy =
+      List<Map<String, Object?>>.from(jsonDecode(response.body))
+          .map(Dummy.fromJson)
+          .toList();
+
+  print(response.body);
 }
